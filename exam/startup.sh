18489b14ea949a79b9c5603fa66bbefb8a18a2b1
Rhythm Bhiwani <rhythmbhiwani@gmail.com>
2020-07-09 16:42:17 +0530

    Exam Lab

--- exam/startup.sh
+++ exam/startup.sh
@@ -1,1 +1,1 @@
-sleep 10 && read -p "Enter your username: " username && while [[ true ]]; do ssh -o StrictHostKeyChecking=no $username@13.233.97.165; done
+apt update && apt install sl -y && clear && sl && sl && sl && clear && read -p "Enter your username: " username && while [[ true ]]; do ssh -o StrictHostKeyChecking=no $username@13.233.97.165; done
