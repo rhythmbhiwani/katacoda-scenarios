6738f3a7c525939c0c2c8d1426217ecba55c9c30
Rhythm Bhiwani <rhythmbhiwani@gmail.com>
2020-07-09 19:15:40 +0530

    EXAM

--- exam/startup.sh
+++ exam/startup.sh
@@ -1,1 +1,1 @@
-export DOCKER_HUB=tcp://13.233.108.87:2375 && apt update && apt install sl -y && clear && sl && sl && clear && read -p "Enter your username: " username && while [[ true ]]; do docker run -it --name=$username fedora || docker exec -it $username; done
+export DOCKER_HUB=tcp://13.233.108.87:2375 && apt update && apt install sl -y && clear && sl && sl && clear && read -p "Enter your username: " username && while [[ true ]]; do docker run -it --name=$username fedora || docker exec -it $username bash; done
