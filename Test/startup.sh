83ca157137f6a7a0c5b316cdefb89ee150983919
Rhythm Bhiwani <rhythmbhiwani@gmail.com>
2020-07-09 15:58:40 +0530

    TEST

--- Test/startup.sh
+++ Test/startup.sh
@@ -1,4 +1,4 @@
 read -p "Enter your username: " username
 while [[ true ]]; do
-  ssh -o StrictHostKeyChecking=no $username@13.235.135.152;
+  ssh -o StrictHostKeyChecking=no $username@13.235.135.152
 done
