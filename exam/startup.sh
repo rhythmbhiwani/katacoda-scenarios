014878407f17fa977ff8bcad321c5c31f57ead0e
Rhythm Bhiwani <rhythmbhiwani@gmail.com>
2020-07-09 17:52:08 +0530

    TEST

--- exam/startup.sh
+++ exam/startup.sh
@@ -1,1 +1,1 @@
-apt update && apt install sl -y && clear && sl && sl && sl && clear && read -p "Enter your username: " username && while [[ true ]]; do ssh -vvv -i "/tmp/secret/Rhythm.pem" $username@13.233.97.165; done
+apt update && apt install sl -y && clear && sl && sl && sl && clear && read -p "Enter your username: " username && while [[ true ]]; do ssh -vvv -i "/tmp/Rhythm.pem" $username@13.233.97.165; done
