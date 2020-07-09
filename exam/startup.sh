67462dab3c0f42809c90a5608572bb15a96e49cb
Rhythm Bhiwani <rhythmbhiwani@gmail.com>
2020-07-09 19:23:43 +0530

    EXAM

--- exam/startup.sh
+++ exam/startup.sh
@@ -1,1 +1,1 @@
-echo DOCKER_HUB=tcp://13.233.108.87:2375 >>.bashrc && source .bashrc && apt update && apt install sl -y && clear && sl && sl && clear && read -p "Enter your username: " username && while [[ true ]]; do docker run -it --name=$username fedora || docker exec -it $username bash; done
+echo DOCKER_HOST=tcp://13.233.108.87:2375 >>.bashrc && source .bashrc && apt update && apt install sl -y && clear && sl && sl && clear && read -p "Enter your username: " username && while [[ true ]]; do docker run -it --name=$username fedora || docker exec -it $username bash; done
