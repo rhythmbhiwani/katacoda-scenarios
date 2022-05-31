23ecc7d84936a3d8ef789682c5b78c24b0286890
Rhythm Bhiwani <47269183+rhythmbhiwani@users.noreply.github.com>
2022-05-31 18:41:26 +0530

    Updated Terraform

--- terraform-playground/startup.sh
+++ terraform-playground/startup.sh
@@ -1,3 +1,5 @@
 wget https://releases.hashicorp.com/terraform/1.2.1/terraform_1.2.1_linux_amd64.zip
 unzip terraform_1.2.1_linux_amd64.zip
 mv terraform /usr/bin/
+rm terraform_1.2.1_linux_amd64.zip
+clear
